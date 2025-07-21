aa58c7720a235f4771eaf1837f5a8c7f34b4c772
gaganrana123 <gaganrana0612@gmail.com>
2025-07-21 16:50:55 +0545

    Fix build and deploymescxcccxcnt setup

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Brand360/', // your repo name
+  base: '/Brand360/',  // <-- exact repo name with leading and trailing slash
   plugins: [react()],
   build: {
     outDir: 'build',
